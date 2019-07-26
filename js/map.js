aeb44d4cf2f8cc43af259154ac8a799df1c676f9
Grigoriy Levanov <glevanov@gmail.com>
2019-07-26 08:52:37 +0300

    Рефакторит движение пина в объект

--- js/map.js
+++ js/map.js
@@ -103,138 +103,149 @@
     return fragment;
   };
 
-  /**
-   * Вставляет пины на карту
-   */
-  var insertPins = function () {
-    var container = map.querySelector('.map__pins');
-    container.appendChild(getTemplatePins(getMockData()));
-  };
-
-  /**
-   * Активирует карту и форму
-   */
-  var activateControls = function () {
-    map.classList.remove('map--faded');
-    window.form.activateForm();
-    controls.forEach(function (control) {
-      control.disabled = false;
-    });
-  };
-
-  /**
-   * Задает адрес в поле адреса
-   */
-  var setAddress = function () {
-    var X = pin.offsetLeft + window.config.Pin.CENTER_OFFSET;
-    var Y = pin.offsetTop + window.config.Pin.CENTER_OFFSET;
-    window.form.address = X + ', ' + Y;
-  };
-
-  /**
-   * Валидирует координату X
-   * @param {number} X Координата
-   * @return {number}
-   */
-  function validateX(X) {
-    if (X < 0) {
-      return 0;
-    } else if (X > window.config.Map.WIDTH - window.config.Pin.RIGHT_OFFSET) {
-      return window.config.Map.WIDTH - window.config.Pin.RIGHT_OFFSET;
-    } else {
-      return X;
-    }
-  }
-
-  /**
-   * Валидирует координату Y
-   * @param {number} Y Координата
-   * @return {number}
-   */
-  function validateY(Y) {
-    if (Y < window.config.Map.MIN_Y) {
-      return window.config.Map.MIN_Y;
-    } else if (Y > window.config.Map.MAX_Y) {
-      return window.config.Map.MAX_Y;
-    } else {
-      return Y;
-    }
-  }
-
-  /**
-   * Обрабатывает mousedown на пин
-   * @param {Object} evt Объект события
-   */
-  var onMouseDown = function (evt) {
-    var pinX = evt.clientX;
-    var pinY = evt.clientY;
-
-    /**
-     * Обрабатывает mousemove на пин
-     * @param {Object} moveEvt Объект события
-     */
-    function onMouseMove(moveEvt) {
-      moveEvt.preventDefault();
-
-      var pinRelativeX;
-      var pinRelativeY;
-
-      function calculatePinPosition() {
-        var NEW_X = pin.offsetLeft - (pinX - moveEvt.clientX);
-        var NEW_Y = pin.offsetTop - (pinY - moveEvt.clientY);
-
-        pinRelativeX = validateX(NEW_X);
-        pinRelativeY = validateY(NEW_Y);
-        pinX = moveEvt.clientX;
-        pinY = moveEvt.clientY;
-      }
-
-      function renderSliderPosition() {
-        pin.style.left = pinRelativeX + 'px';
-        pin.style.top = pinRelativeY + 'px';
-      }
-
-      calculatePinPosition();
-      renderSliderPosition();
-      setAddress();
-    }
-
-    /**
-     * Обрабатывает mouseup на пин
-     * @param {Object} upEvt Объект события
-     */
-    function onMouseUp(upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-      activateMap();
-      setAddress();
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  };
-
-  /**
-   * Активирует карту
-   */
-  var activateMap = function () {
-    if (!isMapActive) {
-      activateControls();
-      insertPins();
-      setAddress();
-      isMapActive = true;
-    }
-  };
-
   var map = document.querySelector('.map');
   var pin = map.querySelector('.map__pin--main');
   var controls = Array.from(document.querySelectorAll('.map__filter, .map__features, .ad-form fieldset'));
-  var isMapActive = false;
 
   window.map = {
+    isMapActive: false,
+    pinX: null,
+    pinY: null,
+    pinRelativeX: null,
+    pinRelativeY: null,
+
+    /**
+     * Вставляет пины на карту
+     */
+    insertPins: function () {
+      var container = map.querySelector('.map__pins');
+      container.appendChild(getTemplatePins(getMockData()));
+    },
+
+    /**
+     * Активирует карту и форму
+     */
+    activateControls: function () {
+      map.classList.remove('map--faded');
+      window.form.activateForm();
+      controls.forEach(function (control) {
+        control.disabled = false;
+      });
+    },
+
+    /**
+     * Валидирует координату X
+     * @param {number} X Координата
+     * @return {number}
+     */
+    validateX: function (X) {
+      if (X < 0) {
+        return 0;
+      } else if (X > window.config.Map.WIDTH - window.config.Pin.RIGHT_OFFSET) {
+        return window.config.Map.WIDTH - window.config.Pin.RIGHT_OFFSET;
+      } else {
+        return X;
+      }
+    },
+
+    /**
+     * Валидирует координату Y
+     * @param {number} Y Координата
+     * @return {number}
+     */
+    validateY: function (Y) {
+      if (Y < window.config.Map.MIN_Y) {
+        return window.config.Map.MIN_Y;
+      } else if (Y > window.config.Map.MAX_Y) {
+        return window.config.Map.MAX_Y;
+      } else {
+        return Y;
+      }
+    },
+
+    /**
+     * Рассчитывает новые координаты пина
+     * @param {Object} evt Объект события
+     */
+    calculatePinPosition: function (evt) {
+      var NEW_X = pin.offsetLeft - (this.pinX - evt.clientX);
+      var NEW_Y = pin.offsetTop - (this.pinY - evt.clientY);
+
+      this.pinRelativeX = this.validateX(NEW_X);
+      this.pinRelativeY = this.validateY(NEW_Y);
+      this.pinX = evt.clientX;
+      this.pinY = evt.clientY;
+    },
+
+    /**
+     * Отрисовывает текущее положение пина
+     */
+    renderSliderPosition: function () {
+      pin.style.left = this.pinRelativeX + 'px';
+      pin.style.top = this.pinRelativeY + 'px';
+    },
+
+    /**
+     * Обрабатывает mousedown на пин
+     * @param {Object} evt Объект события
+     */
+    onMouseDown: function (evt) {
+      this.pinX = evt.clientX;
+      this.pinY = evt.clientY;
+
+      document.addEventListener('mousemove', this.onMouseMove.bind(this));
+      document.addEventListener('mouseup', this.onMouseUp.bind(this));
+    },
+
+    /**
+     * Обрабатывает mousemove на пин
+     * @param {Object} evt Объект события
+     */
+    onMouseMove: function (evt) {
+      evt.preventDefault();
+      this.calculatePinPosition(evt);
+      this.renderSliderPosition();
+      this.setAddress();
+    },
+
+    /**
+     * Обрабатывает mouseup на пин
+     * @param {Object} evt Объект события
+     */
+    onMouseUp: function (evt) {
+      evt.preventDefault();
+      document.removeEventListener('mousemove', this.onMouseMove.bind(this));
+      document.removeEventListener('mouseup', this.onMouseUp.bind(this));
+      this.activateMap();
+      this.setAddress();
+    },
+
+    /**
+     * Задает адрес в поле адреса
+     */
+    setAddress: function () {
+      var X = pin.offsetLeft + window.config.Pin.CENTER_OFFSET;
+      var Y = pin.offsetTop + window.config.Pin.CENTER_OFFSET;
+      window.form.address = X + ', ' + Y;
+    },
+
+    /**
+     * Активирует карту
+     */
+    activateMap: function () {
+      if (!this.isMapActive) {
+        this.activateControls();
+        this.insertPins();
+        this.setAddress();
+        this.isMapActive = true;
+      }
+    },
+
+    /**
+     * Инициализирует карту
+     */
     init: function () {
-      pin.addEventListener('mousedown', onMouseDown);
-    }
+      pin.addEventListener('mousedown', this.onMouseDown.bind(this));
+    },
   };
 })();
